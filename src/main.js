3049016eba93c747433e48b1dfbe3cf297111bc7
Andrés País <andrespais61@gmail.com>
2022-03-14 23:47:46 -0300

    ÂÂÂÂÂÂejercicio terminado

---
+++ src/main.js
@@ -0,0 +1,11 @@
+let vector = new Array(3);
+
+vector[0] = 3;
+vector[1] = 1;
+vector[2] = 7;
+
+document.write(vector[0], '<br>');
+document.write(vector[1], '<br>');
+document.write(vector[2], '<br>');
+
+document.write('la longitud del array es: ', vector.length);
